214e3742c33b5c1ca62702d03bf238d626ef03f9
Hope Lucas <hopelucas17@gmail.com>
2023-09-13 02:12:50 +0930

    ping websocket

--- src/commands/misc/ping.js
+++ src/commands/misc/ping.js
@@ -1,12 +1,16 @@
 module.exports = {
   name: 'ping',
-  description: 'Pong!',
-  // devOnly: Boolean,
-  testOnly: true,
-  // options: Object[],
-  // deleted: Boolean,
+  description: 'Replies with the bot ping!',
 
-  callback: (client, interaction) => {
-    interaction.reply(`Pong! ${client.ws.ping}ms`);
+  callback: async (client, interaction) => {
+    await interaction.deferReply();
+
+    const reply = await interaction.fetchReply();
+
+    const ping = reply.createdTimestamp - interaction.createdTimestamp;
+
+    interaction.editReply(
+      `Pong! Client ${ping}ms | Websocket: ${client.ws.ping}ms`
+    );
   },
 };
