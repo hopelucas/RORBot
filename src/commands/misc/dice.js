5dc408968ae879277c748fa60927ea0b32e548d6
Hope Lucas <hopelucas17@gmail.com>
2023-09-26 00:07:56 +0930

    vcvxc

---
+++ src/commands/misc/dice.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+const {REST, Routes, ApplicationCommandOptionType, EmbedBuilder} = require('discord.js')
+
+module.exports = {
+    name: 'old',
+    description: 'Rolls a dice!',
+    options: [
+        {
+        name: 'dice-sides',
+        description: `The number of sides on your dice.`,
+        type: ApplicationCommandOptionType.Number,
+        required: true,
+        },
+        {
+            name: 'dice-modifiers',
+            description: `Add a modifier?`,
+            type: ApplicationCommandOptionType.Number,
+            required: false,
+        },
+        {
+            name: 'dice-numberof',
+            description: `How many dice should be rolled?`,
+            type: ApplicationCommandOptionType.Number,
+            required: false,
+        },
+    ],
+
+  
+    callback: async (client, interaction) => {
+      await interaction.deferReply();
+
+
+      const num1 = interaction.options.get('dice-sides').value;
+      //const num3 = interaction.options.get('dice-numberof').value;
+      //const num2 = interaction.options.get('dice-modifiers').value;
+
+      // Get first dice rolled
+        //var dice = Math.floor(((Math.random() * num1 + 1) * num3) + num2);
+
+
+    const embed = new EmbedBuilder()
+        .setTitle(`Result: ${dice}!`)
+        .setDescription(`(${num1} * ${num3}) + ${num2}`)
+        .setColor('Random')
+
+      interaction.editReply({embeds: [embed]});
+
+    },
+  };
