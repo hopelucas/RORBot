2a6bfd8188b39b330dc769df954c22c7b469a422
Hope Lucas <hopelucas17@gmail.com>
2023-09-25 17:22:26 +0930

    module

--- src/commands/misc/dice.js
+++ src/commands/misc/dice.js
@@ -1,11 +1,12 @@
 module.exports = {
     name: 'lettuce',
     description: 'Replies with the bot ping!',
+    type: ApplicationCommandOptionType.Number,
+
   
     callback: async (client, interaction) => {
       await interaction.deferReply();
-      const num1 = interaction.options.get('Sides').value;
-        var rating = Math.floor(Math.random() * num1 + 1);
+        var rating = Math.floor(Math.random() * 20 + 1);
       interaction.editReply(
         `AAAAA MY PENIS EXPLODE ${rating} times!`
       );
