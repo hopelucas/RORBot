2ed0a484936a387f12acb49ebbf093573882c2d2
Hope Lucas <hopelucas17@gmail.com>
2023-09-25 16:48:15 +0930

    embed rando

--- src/commands/misc/dice.js
+++ src/commands/misc/dice.js
@@ -1,25 +1,29 @@
 module.exports = {
-    name: 'random',
-    description: 'Replies with the bot ping!',
-
-execute(message, args, Discord){
-    if (!args.length ||!args[1]){
+    name: 'rand',
+    description: "generates a random number",
+    execute(message, args, Discord) {
+  
+      if (!args.length || !args[1]) {
         message.channel.send("You need to type in the min and max values like this \`!rand 10 5\`")
-
-    }
-    else {
+      }
+  
+      else {
+  
         let max = args[0]
         let min = args[1]
-
-        let randNo = Math.round(Math.random() * max + min)
-
-        const embed = new Discord.MessageEmbed()
-        .setTitle("Your random number generated is:")
-        .setDescription(randNo)
-        .setColor("PINK")
-        .setFooter("Random Number Generator")
-
-        message.reply(embed)
+  
+      let randNo = Math.round(Math.random() * max + min)
+  
+      const embed = new Discord.MessageEmbed()
+      .setTitle("Your random number is: ")
+      .setDescription(randNo)
+      .setColor("YELLOW")
+      .setFooter("Random Number Generator")
+      .setTimestamp()
+  
+      message.reply(embed)
+  
+      }
+  
     }
-}
-};
+  }
