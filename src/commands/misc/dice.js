5555b1fedbd86b0c8003ce9a8f8420d69d3ca4ce
Hope Lucas <hopelucas17@gmail.com>
2023-09-25 16:45:14 +0930

    random number generator?

--- src/commands/misc/dice.js
+++ src/commands/misc/dice.js
@@ -1,12 +1,25 @@
-var array = [1, 2, 3, 4, 5];
-
 module.exports = {
-    name: 'roll',
+    name: 'random',
     description: 'Replies with the bot ping!',
 
+execute(message, args, Discord){
+    if (!args.length ||!args[1]){
+        message.channel.send("You need to type in the min and max values like this \`!rand 10 5\`")
 
-  get random_array_member() {
-    return array[Math.floor(Math.random()*array.length)]
-  }
+    }
+    else {
+        let max = args[0]
+        let min = args[1]
+
+        let randNo = Math.round(Math.random() * max + min)
+
+        const embed = new Discord.MessageEmbed()
+        .setTitle("Your random number generated is:")
+        .setDescription(randNo)
+        .setColor("PINK")
+        .setFooter("Random Number Generator")
+
+        message.reply(embed)
+    }
 }
-    
+};
