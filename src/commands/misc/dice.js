54d4d6f37cf564ec49bf04f50b798bd54399237d
Hope Lucas <hopelucas17@gmail.com>
2023-09-25 16:09:51 +0930

    dice again

--- src/commands/misc/dice.js
+++ src/commands/misc/dice.js
@@ -1,4 +1,41 @@
-const DiceRoller = require('../node_modules/dice-roller/dice-roller.js');
+module.exports = { 
+  name: 'roll', 
+  description: 'Roll\'s a die', 
+  args: true, 
+  usage: ['<die-size>', '<number-of-rolls>d<die-size>'], 
+  aliases: ['rol', 'nat20'], authorizedChannels:['general'], 
+   execute(message, args) { 
+     let arg = args[0]; 
+     let modifier = 0; 
+     let resMessage = ''; 
+     if(/^.*\+[0-9]+$/.test(arg)) { 
+       const rollAndModifier = arg.split('+'); arg = rollAndModifier[0]; 
+       modifier = rollAndModifier[1]; 
+     } 
+     if (/^[0-9]+d[0-9]+$/.test(arg)){ 
+        rollInfo = arg.split('d'); 
+        numRolls = parseInt(rollInfo[0]); 
+        dieSize = parseInt(rollInfo[1]); 
+        const resRolls = Array.apply(null, {length: numRolls}).map((_) => { 
+           return Math.floor(Math.random()*dieSize) + 1; 
+         }); 
+         resMessage = `rolled: ${resRolls.join(",")}`; 
+      } else { 
+         if(/^d[0-9]+$/.test(arg)) { 
+           arg = arg.replace('d', ''); 
+         } 
+         const dieSize = parseInt(arg); 
+         if (isNaN(dieSize)) { 
+           return message.reply('Invalid roll'); 
+         } 
+         resMessage = `rolled ${Math.floor(Math.random()*dieSize) + 1}`; 
+       } 
+       if(modifier > 0) { 
+         resMessage += ` +${modifier}`; 
+       } 
+     return message.reply(resMessage); 
+   }, 
+ };const DiceRoller = require('../node_modules/dice-roller/dice-roller.js');
 
 module.exports = {
 	name: 'roll',
@@ -19,6 +56,5 @@ module.exports = {
 		let reply = `${message.author} rolled ${result.toString()}`;
 
 		return message.channel.send(reply).catch(console.error);
-
 	},
 };
