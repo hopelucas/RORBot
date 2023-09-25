c0128bd68f9c25da735c69a535b9b2d362dfd083
Hope Lucas <hopelucas17@gmail.com>
2023-09-25 17:50:48 +0930

    fggfhf

--- src/commands/misc/dice.js
+++ src/commands/misc/dice.js
@@ -29,11 +29,11 @@ module.exports = {
     callback: async (client, interaction) => {
       await interaction.deferReply();
       const num1 = interaction.options.get('dice-sides').value;
-      const num2 = interaction.options.get('dice-modifiers')?.value;
       const num3 = interaction.options.get('dice-numberof').value;
+      const num2 = interaction.options.get('dice-modifiers')?.value;
         var rating = Math.floor((Math.random() * num1 + 1 + num2) * num3);
       interaction.editReply(
-        `You rolled a ${rating} - out of a possible ${num1}!`
+        `You rolled a ${rating} - Sides: ${num1}!, Number of Dice: ${num3}, Modifiers: ${num2}`
       );
     },
   };
