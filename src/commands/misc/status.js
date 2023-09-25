09e6b48719a3287be27c2c6d2975eda2876c8a9a
Hope Lucas <hopelucas17@gmail.com>
2023-09-25 19:09:50 +0930

    jkjkjhk

--- src/commands/misc/status.js
+++ src/commands/misc/status.js
@@ -1,4 +1,8 @@
+require('dotenv').config();
+const { Client, IntentsBitField, ActivityType } = require('discord.js');
+
 module.exports = (client) => {
+
     console.log('Natsuki is online!');
     client.user.setActivity('my status', { type: 'PLAYING' })
 }
