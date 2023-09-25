a70ee2165d35ee65923d941e8f26c70f9d9eaa2c
Hope Lucas <hopelucas17@gmail.com>
2023-09-25 17:35:38 +0930

    shouldnt crash

--- src/commands/misc/dice.js
+++ src/commands/misc/dice.js
@@ -1,11 +1,23 @@
+require('dotenv').config();
+const {REST, Routes, ApplicationCommandOptionType} = require('discord.js')
+
 module.exports = {
     name: 'lettuce',
     description: 'Replies with the bot ping!',
+    options: [
+        {
+        name: 'first-number',
+        description: `The first number`,
+        type: ApplicationCommandOptionType.Number,
+        required: true,
+        },
+    ],
 
   
     callback: async (client, interaction) => {
       await interaction.deferReply();
-        var rating = Math.floor(Math.random() * 20 + 1);
+      const num1 = interaction.options.get('first-number').value;
+        var rating = Math.floor(Math.random() * num1 + 1);
       interaction.editReply(
         `AAAAA MY PENIS EXPLODE ${rating} times!`
       );
