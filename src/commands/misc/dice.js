e9c956b86f0698d6a7d1ec12270321052b878945
Hope Lucas <hopelucas17@gmail.com>
2023-09-25 18:01:40 +0930

    embedded

--- src/commands/misc/dice.js
+++ src/commands/misc/dice.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-const {REST, Routes, ApplicationCommandOptionType} = require('discord.js')
+const { default: Description } = require('@dice-roller/rpg-dice-roller/types/Description');
+const {REST, Routes, ApplicationCommandOptionType, EmbedBuilder} = require('discord.js')
 
 module.exports = {
     name: 'dice',
@@ -28,12 +29,17 @@ module.exports = {
   
     callback: async (client, interaction) => {
       await interaction.deferReply();
+
+      const embed = new EmbedBuilder()
+        .setTitle('Embed Title')
+        .setDescription(`You rolled a ${rating} - Sides: ${num1}!, Number of Dice: ${num3}, Modifiers: ${num2}`)
+
+
       const num1 = interaction.options.get('dice-sides').value;
       const num3 = interaction.options.get('dice-numberof').value;
       const num2 = interaction.options.get('dice-modifiers')?.value;
         var rating = Math.floor((Math.random() * num1 + 1 + num2) * num3);
-      interaction.editReply(
-        `You rolled a ${rating} - Sides: ${num1}!, Number of Dice: ${num3}, Modifiers: ${num2}`
-      );
+      interaction.editReply({embeds: [embed]});
+
     },
   };
