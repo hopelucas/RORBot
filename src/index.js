5c112f0c95b1d8ee80d845d392e6031ff50f3ccf
Hope Lucas <hopelucas17@gmail.com>
2023-09-25 19:01:53 +0930

    dfglkjfdgm,kfd

--- src/index.js
+++ src/index.js
@@ -11,6 +11,34 @@ const client = new Client({
   ],
 });
 
+let status = [
+  {
+    name: 'Under Ctrl',
+    type: ActivityType.Streaming,
+    url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+  },
+  {
+    name: 'Custom Status 1',
+  },
+  {
+    name: 'Custom Status 2',
+    type: ActivityType.Watching,
+  },
+  {
+    name: 'Custom Status 3',
+    type: ActivityType.Listening,
+  },
+];
+
+client.on('ready', (c) => {
+  console.log(`✅ ${c.user.tag} is online.`);
+
+  setInterval(() => {
+    let random = Math.floor(Math.random() * status.length);
+    client.user.setActivity(status[random]);
+  }, 10000);
+});
+
 
 eventHandler(client);
 
