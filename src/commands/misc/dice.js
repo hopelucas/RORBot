81c8ccd1a58dd071f043baf963483bf529031630
Hope Lucas <hopelucas17@gmail.com>
2023-09-25 17:47:06 +0930

    howmanydice

--- src/commands/misc/dice.js
+++ src/commands/misc/dice.js
@@ -11,13 +11,27 @@ module.exports = {
         type: ApplicationCommandOptionType.Number,
         required: true,
         },
+        {
+            name: 'dice-modifiers',
+            description: `Add a modifier?`,
+            type: ApplicationCommandOptionType.Number,
+            required: false,
+        },
+        {
+            name: 'dice-numberof',
+            description: `How many dice should be rolled?`,
+            type: ApplicationCommandOptionType.Number,
+            required: false,
+        },
     ],
 
   
     callback: async (client, interaction) => {
       await interaction.deferReply();
       const num1 = interaction.options.get('dice-sides').value;
-        var rating = Math.floor(Math.random() * num1 + 1);
+      const num2 = interaction.options.get('dice-modifiers')?.value;
+      const num3 = interaction.options.get('dice-numberof').value;
+        var rating = Math.floor((Math.random() * num1 + 1 + num2) * num3);
       interaction.editReply(
         `You rolled a ${rating} - out of a possible ${num1}!`
       );
