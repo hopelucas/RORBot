fb93f2b0ea144e7fe55c46f404c5ed89cca93a19
Hope Lucas <hopelucas17@gmail.com>
2023-09-25 19:18:56 +0930

    fgfddgfdg

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 const { Client, IntentsBitField } = require('discord.js');
 const eventHandler = require('./handlers/eventHandler');
 
+client.user.setActivity('your status', {
+ type: 'a type like STREAMING',
+ url: 'url of streaming',
+});
+
 const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
