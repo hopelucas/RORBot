a23f3454af6b7432eb6c3846fc27cb954bfbadf9
Hope Lucas <hopelucas17@gmail.com>
2023-09-25 18:22:22 +0930

    dice roll success

--- src/commands/misc/dice.js
+++ src/commands/misc/dice.js
@@ -33,20 +33,21 @@ module.exports = {
       const num1 = interaction.options.get('dice-sides').value;
       const num3 = interaction.options.get('dice-numberof').value;
       const num2 = interaction.options.get('dice-modifiers')?.value;
-        var rating = Math.floor((Math.random() * num1 + 1 + num2) * num3);
+        var rating = Math.floor(((Math.random() * num1 + 1) * num3) + num2);
 
     const embed = new EmbedBuilder()
         .setTitle(`Result: ${rating}!`)
         .setDescription(`You roll some dice.`)
         .setColor('Random')
         .addFields({
-            name: `Sides: ${num1}`,
+            name: `Sides:`,
             inline: true,
         }, {
-            name: `Number of Dice: ${num3}`,
+            name: `Number of Dice:`,
+            value: num3,
             inline: true,
         }, {
-            name: `Modifiers: ${num2}`,
+            name: `Modifiers:`,
             inline: true,
         },)
 
