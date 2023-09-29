443c16b8f61de02857d2a19461bc01522b7cc110
Hope Lucas <hopelucas17@gmail.com>
2023-09-29 15:56:51 +0930

    Try this

--- src/commands/misc/ping.js
+++ src/commands/misc/ping.js
@@ -1,3 +1,13 @@
+const {
+    ButtonStyle,
+    EmbedBuilder,
+    ButtonBuilder,
+    ActionRowBuilder,
+    ChatInputCommandInteraction,
+    ApplicationCommandOptionType,
+} = require('discord.js');require('dotenv/config');
+const { Client, IntentsBitField, ActivityType } = require('discord.js');
+
 const data = {
     name: 'ping',
     description: 'Pong!',
